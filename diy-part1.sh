f0893902ae0770ba88299aec853eb86b504662be
Git IY! <91715524+gitiy1@users.noreply.github.com>
2025-02-19 12:47:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,8 +2,31 @@
 #
 # Thanks for https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (After Update feeds)
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+
+echo '
+
+define KernelPackage/xdp-sockets-diag
+  SUBMENU:=$(NETWORK_SUPPORT_MENU)
+  TITLE:=PF_XDP sockets monitoring interface support for ss utility
+  KCONFIG:= \
+	CONFIG_XDP_SOCKETS=y \
+	CONFIG_XDP_SOCKETS_DIAG
+  FILES:=$(LINUX_DIR)/net/xdp/xsk_diag.ko
+  AUTOLOAD:=$(call AutoLoad,31,xsk_diag)
+endef
+
+define KernelPackage/xdp-sockets-diag/description
+ Support for PF_XDP sockets monitoring interface used by the ss tool
+endef
+
+$(eval $(call KernelPackage,xdp-sockets-diag))
+' >> package/kernel/linux/modules/netsupport.mk
+
+git clone https://github.com/sbwml/luci-app-mentohust package/mentohust
+git clone --depth 1  https://github.com/217heidai/OpenWrt-Packages.git package/custom
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
