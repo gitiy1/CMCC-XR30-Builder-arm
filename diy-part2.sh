692eadde742aec6eb58d2463eaf105a1a1d9a1a8
Git IY! <91715524+gitiy1@users.noreply.github.com>
2025-02-19 10:39:57 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -247,7 +247,7 @@ config_package_add kmod-xdp-sockets-diag
 git clone https://github.com/QiuSimons/luci-app-daed package/dae
 mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
 config_package_add luci-app-daed
-config_package_del rd05a1
+echo "CONFIG_PACKAGE_rd05a1=n" >> .config
 make package/dae/luci-app-daed/compile -j$(($(nproc) + 1)) V=s
 
 # ## Frp Latest version patch
